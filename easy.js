4d65f45deb7cff911a263fe127afed7d431e8299
Patrick_B <pb5191@gmail.com>
2018-10-24 05:04:44 -0400

    added isPalindrome & twoNumSum in easy.js

---
+++ easy.js
@@ -0,0 +1,48 @@
+// isPalindrome
+
+// input: a string, output: a Boolean
+// function which checks if a string can be read
+// backwards as well as forward
+
+// iteration
+const isPalindrome = str => {
+  let string = str
+    .replace(/[^\w]/gi, "") // replace all non-words characters
+    .toLowerCase(); // make all characters lower cased
+  return (
+    string ===
+    string
+      .split("")
+      .reverse()
+      .join("")
+  );
+};
+
+// recursion
+const isPalindrome = str => {
+  let len = str.length;
+  if (len < 2) return true;
+  if (str[0] === str[len - 1]) return isPalindrome(str.slice(1, len - 1));
+  return false;
+};
+
+//******************************************************************* */
+// Two-numbers Sum
+
+// APPROACH: Brute force ==> 2 loops but O(n square) in time
+// Other strategy ==> a hash table and storing the current value.
+// Hash table because it has a O(1) look-up time.
+// time and space complexity is then O(n)
+// EDGE CASES are array.length = 1 or 2, 2 equal values in array can be used to get targetsum
+// Ex: [3, -1, 0, 3] & targetsum is 6
+
+const twoNumSum = (arr, target) => {
+  const ref = {};
+  for (const num of arr) {
+    const diff = target - num;
+    if (diff in ref) {
+      return [num, diff].sort((a, b) => a - b);
+    }
+  }
+  return [];
+};
