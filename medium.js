8b2bccebc4d739efdcdc86734daf250804727d49
Patrick_B <pb5191@gmail.com>
2019-01-25 18:55:52 -0500

    added Smallest Difference to medium.js

--- medium.js
+++ medium.js
@@ -7,7 +7,6 @@ Sample input: [12, 3, 1, 2, -6, 5, -8, 6], 0
 Sample output: [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
 */
 
-
 // Approach:
 // Brute force ==> 3 loops, and O(n^3) time
 // 1- Sort the input array
@@ -34,12 +33,21 @@ const threeNumSum = (arr, target) => {
         triplets.push(current);
         left++;
         right--;
-      }
-      else if (currentSum < target) left++;
+      } else if (currentSum < target) left++;
       else if (currentSum > target) right--;
     }
   }
   return triplets;
-}
+};
 
 //******************************************************************* */
+// Smallest Difference
+
+// Write a function that takes in two non-empty arrays of integers. The function should find the pair of numbers (one from the first array, one from the second array) whose absolute difference is closest to zero. The function should return an array containing these two numbers, with the number from the first array in the first position. Assume that there will only be one pair of numbers with the smallest difference.
+
+// Sample input: [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]
+// Sample output: [28, 26] ==> smallest difference is 2
+
+// Approach:
+// First sort the arrays.
+// Then use 1 pointer per array, starting at 0.
