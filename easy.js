0856ab27c994913d160ce8b4f952fccec107f544
Patrick_B <pb5191@gmail.com>
2019-11-09 10:00:10 -0500

    added remove dupes from sorted array in place

--- easy.js
+++ easy.js
@@ -510,3 +510,41 @@ var mergeTwoLists = function(l1, l2) {
   currNode.next = l1 || l2;
   return head.next;
 };
+
+//Remove duplicates from sorted Array
+
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+Given nums = [1,1,2],
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the returned length.
+
+Example 2:
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+It doesn't matter what values are set beyond the returned length.
+*/
+
+// const test1 = []; // ==> 0
+// const test2 = [1, 1, 2]; // ==> 2
+// const test3 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]; // ==> 5
+// const test4 = [0, 0, 0, 0, 0]; // ==> 1
+// const test5 = [0]; // ==> 1
+// const test6 = [-1, 0, 0, 0, 0, 3, 3]; // ==> 3
+
+const removeDupes = nums => {
+  if (nums.length > 0) {
+    let counter = 0;
+    while (counter < nums.length) {
+      if (nums[counter] === nums[counter + 1]) {
+        nums.splice(counter + 1, 1);
+      } else {
+        counter++;
+      }
+    }
+  }
+  return nums.length;
+};
