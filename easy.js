dcb65612bc5b06c4a7f9fa3701fafff5ab4b1a82
Patrick_B <pb5191@gmail.com>
2019-11-17 10:00:39 -0500

    added searchInsert()

--- easy.js
+++ easy.js
@@ -629,3 +629,32 @@ const strStr = (haystack, needle) => {
   }
   return result;
 };
+
+// Search Insert
+
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+*/
+
+// const test1 = [1, 3, 5, 6],
+//   val1 = 5; // ==> 2
+// const test2 = [1, 3, 5, 6],
+//   val2 = 2; // ==> 1
+// const test3 = [1, 3, 5, 6],
+//   val3 = 7; // ==> 4
+// const test4 = [1, 3, 5, 6],
+//   val4 = 0; // ==> 0
+
+const searchInsert = (nums, target) => {
+  let index = nums.length;
+  if (target >= nums[0] && target <= nums[nums.length - 1]) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === target) index = i;
+      if (nums[i] < target && nums[i + 1] > target) index = i + 1;
+    }
+  } else {
+    if (target < nums[0]) index = 0;
+  }
+  return index;
+};
--- test-file.js
+++ test-file.js
@@ -1,14 +1,27 @@
-const test1 = "hello",
-  val1 = "ll"; // ==> 2
-const test2 = "hello",
-  val2 = ""; // ==> 0  test can be any string
-const test3 = "aaaaaaaaa",
-  val3 = "bba"; // ==> -1
-const test4 = "",
-  val4 = "q"; // ==> 0
+const test1 = [1, 3, 5, 6],
+  val1 = 5; // ==> 2
+const test2 = [1, 3, 5, 6],
+  val2 = 2; // ==> 1
+const test3 = [1, 3, 5, 6],
+  val3 = 7; // ==> 4
+const test4 = [1, 3, 5, 6],
+  val4 = 0; // ==> 0
 
-const strStr = (haystack, needle) => {
-  return haystack.search(needle);
+const searchInsert = (nums, target) => {
+  let index = nums.length;
+  if (target >= nums[0] && target <= nums[nums.length - 1]) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === target) index = i;
+      if (nums[i] < target && nums[i + 1] > target) index = i + 1;
+    }
+  } else {
+    if (target < nums[0]) index = 0;
+  }
+
+  return index;
 };
 
-console.log(strStr(test4, val4));
+console.log(searchInsert(test1, val1));
+console.log(searchInsert(test2, val2));
+console.log(searchInsert(test3, val3));
+console.log(searchInsert(test4, val4));
